76f8c8796ce0268ab74a614f60a898d30ad1b6b9
c-bata <contact@c-bata.link>
2017-07-18 20:02:30 +0900

    Remove executor.go

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"os/exec"
+	"strings"
+
 	"github.com/c-bata/go-prompt-toolkit"
 	"github.com/c-bata/kube-prompt/kube"
 )
 
+func executor(s string) string {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return ""
+	}
+
+	args := strings.Split(s, " ")
+	cmd := exec.Command("kubectl", args...)
+	out, err := cmd.Output()
+	if err != nil {
+		return err.Error()
+	}
+	return string(out)
+}
+
 func main() {
 	pt := prompt.NewPrompt(
-		kube.Executor,
+		executor,
 		kube.Completer,
 		prompt.OptionTitle("kube-prompt: powerful kubernetes client"),
 		prompt.OptionPrefix(">>> "),
