f28fe08522cb38012e17ae795e702c35c9ac9553
Sol Cates <scates@vormetric.com>
2017-08-17 14:37:39 -0700

    added support for 'config use-context <completion>'... hacky but functional

--- kube/executor.go
+++ kube/executor.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"bytes"
 )
 
 func Executor(s string) {
@@ -25,4 +26,23 @@ func Executor(s string) {
 		fmt.Printf("Got error: %s\n", err.Error())
 	}
 	return
+}
+
+func ExecuteAndReturnList(s string) (list []string) {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return
+	}
+
+	out := &bytes.Buffer{}
+	cmd := exec.Command("/bin/sh", "-c", "kubectl "+s)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = out
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Got error: %s\n", err.Error())
+	}
+
+	ss := string(out.Bytes())
+	return strings.Split(ss, "\n")
+
 }
