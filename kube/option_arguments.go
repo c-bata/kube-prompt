1ef952a32ea42732020cb6e0c54c17f5c08294e3
c-bata <contact@c-bata.link>
2017-10-13 18:19:43 +0900

    Fix a conflict of apply -f with logs -f options

--- kube/option_arguments.go
+++ kube/option_arguments.go
@@ -14,30 +14,33 @@ func init() {
 	fileListCache = map[string][]prompt.Suggest{}
 }
 
-func getPreviousOption(d prompt.Document) (option string, found bool) {
+func getPreviousOption(d prompt.Document) (cmd, option string, found bool) {
 	args := strings.Split(d.TextBeforeCursor(), " ")
 	l := len(args)
 	if l >= 2 {
 		option = args[l-2]
 	}
 	if strings.HasPrefix(option, "-") {
-		return option, true
+		return args[0], option, true
 	}
-	return "", false
+	return "", "", false
 }
 
 func completeOptionArguments(d prompt.Document) ([]prompt.Suggest, bool) {
-	option, found := getPreviousOption(d)
+	cmd, option, found := getPreviousOption(d)
 	if !found {
 		return []prompt.Suggest{}, false
 	}
-
-	switch option {
-	case "-f", "--filename":
-		return fileCompleter(d), true
-	default:
-		return []prompt.Suggest{}, false
+	switch cmd {
+	case "get", "describe", "create", "delete", "replace", "patch",
+		"edit", "apply", "expose", "rolling-update", "rollout",
+		"label", "annotate", "scale", "convert", "autoscale":
+		switch option {
+		case "-f", "--filename":
+			return fileCompleter(d), true
+		}
 	}
+	return []prompt.Suggest{}, false
 }
 
 /* file list */
@@ -46,6 +49,9 @@ var fileListCache map[string][]prompt.Suggest
 
 func fileCompleter(d prompt.Document) []prompt.Suggest {
 	path := d.GetWordBeforeCursor()
+	if strings.HasPrefix(path, "./") {
+		path = path[2:]
+	}
 	dir := filepath.Dir(path)
 	if cached, ok := fileListCache[dir]; ok {
 		return cached
