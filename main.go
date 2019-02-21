fa7ef4fe0000bbeef8a2b0e022d23f44d83af5b7
c-bata <contact@c-bata.link>
2019-02-21 23:04:43 +0900

    Support oidc and openstack plugin

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/c-bata/kube-prompt/kube"
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
 
 var (
