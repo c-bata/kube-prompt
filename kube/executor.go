44a8b221cb67a146e25c4e7337f0c06b97b02634
mic4ael <crossner90@gmail.com>
2020-10-17 11:54:49 +0200

    Colorize yaml/json output using chroma

--- kube/executor.go
+++ kube/executor.go
@@ -7,29 +7,78 @@ import (
 	"os/exec"
 	"strings"
 
+	"github.com/alecthomas/chroma/quick"
 	"github.com/c-bata/kube-prompt/internal/debug"
+	"github.com/efekarakus/termcolor"
+
+	"github.com/google/shlex"
 )
 
-func Executor(s string) {
-	s = strings.TrimSpace(s)
-	if s == "" {
-		return
-	} else if s == "quit" || s == "exit" {
-		fmt.Println("Bye!")
-		os.Exit(0)
-		return
-	}
+func getOutputLexerType(kubectlCmd string) (lexerType string) {
+	kubectlArgs, _ := shlex.Split(kubectlCmd)
 
-	cmd := exec.Command("/bin/sh", "-c", "kubectl "+s)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		fmt.Printf("Got error: %s\n", err.Error())
+	for index, arg := range kubectlArgs {
+		if arg == "-o" {
+			lexerType = strings.ToLower(kubectlArgs[index+1])
+		} else if strings.HasPrefix(arg, "--output=") {
+			lexerType = strings.ToLower(strings.Split(arg, "=")[1])
+		}
 	}
 	return
 }
 
+func getOutputFormatter() (formatter string) {
+	switch termColor := termcolor.SupportLevel(os.Stdout); termColor {
+	case termcolor.Level16M:
+		formatter = "terminal16m"
+	case termcolor.Level256:
+		formatter = "terminal256"
+	case termcolor.LevelBasic:
+		formatter = "terminal"
+	case termcolor.LevelNone:
+	default:
+		formatter = ""
+	}
+	return
+}
+
+// Executor passed to go-prompt
+func Executor(style string) func(string) {
+	return func(s string) {
+		s = strings.TrimSpace(s)
+		if s == "" {
+			return
+		} else if s == "quit" || s == "exit" {
+			fmt.Println("Bye!")
+			os.Exit(0)
+			return
+		}
+
+		output := &bytes.Buffer{}
+		cmd := exec.Command("/bin/sh", "-c", "kubectl "+s)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = output
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			fmt.Printf("Got error: %s\n", err.Error())
+		} else {
+			highlighted := &bytes.Buffer{}
+			lexerType := getOutputLexerType(s)
+			formatterType := getOutputFormatter()
+
+			if formatterType != "" && lexerType != "" && (lexerType == "json" || lexerType == "yaml") {
+				quick.Highlight(highlighted, output.String(), lexerType, formatterType, style)
+				os.Stdout.Write(highlighted.Bytes())
+			} else {
+				os.Stdout.Write(output.Bytes())
+			}
+		}
+		return
+	}
+}
+
+// ExecuteAndGetResult Execute the command provided by the argument and return the stdout
 func ExecuteAndGetResult(s string) string {
 	s = strings.TrimSpace(s)
 	if s == "" {
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
+	"github.com/alecthomas/chroma/styles"
 	prompt "github.com/c-bata/go-prompt"
 	"github.com/c-bata/go-prompt/completer"
 	"github.com/c-bata/kube-prompt/internal/debug"
@@ -17,9 +19,28 @@ import (
 var (
 	version  string
 	revision string
+	style    string
 )
 
+func isStyleSupported(item string) bool {
+	for _, availableStyle := range styles.Names() {
+		if item == availableStyle {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
+	styleUsage := fmt.Sprintf("The chroma style to use when coloring json/yaml output. Available options: %s", styles.Names())
+	flag.StringVar(&style, "style", "algol", styleUsage)
+	flag.Parse()
+
+	if !isStyleSupported(style) {
+		fmt.Fprintf(os.Stderr, "%s style does not exist\n", style)
+		os.Exit(1)
+	}
+
 	c, err := kube.NewCompleter()
 	if err != nil {
 		fmt.Println("error", err)
@@ -31,7 +52,7 @@ func main() {
 	fmt.Println("Please use `exit` or `Ctrl-D` to exit this program.")
 	defer fmt.Println("Bye!")
 	p := prompt.New(
-		kube.Executor,
+		kube.Executor(style),
 		c.Complete,
 		prompt.OptionTitle("kube-prompt: interactive kubernetes client"),
 		prompt.OptionPrefix(">>> "),
